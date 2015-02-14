4facd69466cbb9e4788d38f20cf5dd2dd8980b02
nghialv <levan_nghia@cyberagent.co.jp>
2015-02-14 23:35:07 +0900

    implement section and manager header/footer in section

---
+++ Source/MYSection.swift
@@ -0,0 +1,9 @@
+//
+//  MYSection.swift
+//  MYTableViewManager
+//
+//  Created by Le VanNghia on 2/14/15.
+//  Copyright (c) 2015 Le Van Nghia. All rights reserved.
+//
+
+import Foundation
---
+++ Source/MYViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MYViewModel.swift
+//  MYTableViewManager
+//
+//  Created by Le VanNghia on 2/14/15.
+//  Copyright (c) 2015 Le Van Nghia. All rights reserved.
+//
+
+import Foundation
